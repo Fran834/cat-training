0bdccbb5559b197a2fb9df2531895f81af5e93be
Fran834 <fran-834@hotmail.com>
2022-01-26 11:02:24 +0000

    Rellenar base datos

--- meta/postgres_25012022.sql
+++ meta/postgres_25012022.sql
@@ -1,4 +1,8 @@
-CREATE SEQUENCE expenses_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1;
+-- Adminer 4.8.1 PostgreSQL 14.1 (Debian 14.1-1.pgdg110+1) dump
+
+DROP TABLE IF EXISTS "expenses";
+DROP SEQUENCE IF EXISTS expenses_id_seq;
+CREATE SEQUENCE expenses_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 5 CACHE 1;
 
 CREATE TABLE "public"."expenses" (
     "id" integer DEFAULT nextval('expenses_id_seq') NOT NULL,
@@ -12,29 +16,15 @@ CREATE TABLE "public"."expenses" (
 COMMENT ON TABLE "public"."expenses" IS 'Store my money';
 
 INSERT INTO "expenses" ("id", "user_id", "created_at", "amount", "notes") VALUES
-(1,	1,	'2022-01-25 16:52:13.065486+00',	10,	'Nota 1'),
-(2,	2,	'2022-01-25 16:52:26.288524+00',	55,	'Notas 2'),
-(3,	2,	'2022-01-25 16:52:50.574111+00',	15,	'Colores');
+(1,	2,	'2022-01-25 16:52:08.737986+00',	22,	'first note'),
+(2,	2,	'2022-01-25 16:52:15.4356+00',	30,	'third note'),
+(3,	3,	'2022-01-25 16:52:21.991514+00',	10,	'apples'),
+(4,	4,	'2022-01-25 16:52:28.441628+00',	99,	'computers'),
+(5,	4,	'2022-01-25 16:52:33.541738+00',	20,	'train');
 
-<br />
-<b>Warning</b>:  PDO::query(): SQLSTATE[42703]: Undefined column: 7 ERROR:  column &quot;consrc&quot; does not exist
-LINE 1: SELECT conname, consrc
-                        ^
-HINT:  Perhaps you meant to reference the column &quot;pg_constraint.conkey&quot; or the column &quot;pg_constraint.conbin&quot;. in <b>/var/www/html/adminer.php</b> on line <b>185</b><br />
-CREATE TABLE "public"."foobar" (
-    "ff" integer NOT NULL
-) WITH (oids = false);
-
-INSERT INTO "foobar" ("ff") VALUES
-(11),
-(22);
-
-<br />
-<b>Warning</b>:  PDO::query(): SQLSTATE[42703]: Undefined column: 7 ERROR:  column &quot;consrc&quot; does not exist
-LINE 1: SELECT conname, consrc
-                        ^
-HINT:  Perhaps you meant to reference the column &quot;pg_constraint.conkey&quot; or the column &quot;pg_constraint.conbin&quot;. in <b>/var/www/html/adminer.php</b> on line <b>185</b><br />
-CREATE SEQUENCE users_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 1 CACHE 1;
+DROP TABLE IF EXISTS "users";
+DROP SEQUENCE IF EXISTS users_id_seq;
+CREATE SEQUENCE users_id_seq INCREMENT 1 MINVALUE 1 MAXVALUE 2147483647 START 4 CACHE 1;
 
 CREATE TABLE "public"."users" (
     "id" integer DEFAULT nextval('users_id_seq') NOT NULL,
@@ -43,12 +33,13 @@ CREATE TABLE "public"."users" (
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 ) WITH (oids = false);
 
-COMMENT ON TABLE "public"."users" IS 'it contents my users';
+COMMENT ON TABLE "public"."users" IS 'It contains my users';
 
 INSERT INTO "users" ("id", "name", "created_at") VALUES
-(1,	'Fran',	'2022-01-25 16:38:35.419017+00'),
-(2,	'Marco',	'2022-01-25 16:41:53.108061+00');
+(2,	'silvia',	'2022-01-25 16:38:35.902582+00'),
+(3,	'franc',	'2022-01-25 16:38:43.302965+00'),
+(4,	'antonio',	'2022-01-25 16:42:44.55125+00');
 
 ALTER TABLE ONLY "public"."expenses" ADD CONSTRAINT "expenses_user_id_fkey" FOREIGN KEY (user_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT DEFERRABLE;
 
--- 2022-01-25 17:06:17.741205+00
+-- 2022-01-25 17:05:07.520544+00
