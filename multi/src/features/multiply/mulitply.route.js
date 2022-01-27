e77e566b067a2d758d751f3b0ba483b9a93ccae1
Fran834 <fran-834@hotmail.com>
2022-01-27 10:11:13 +0000

    Docker

--- multi/src/features/multiply/mulitply.route.js
+++ multi/src/features/multiply/mulitply.route.js
@@ -3,22 +3,22 @@ const multiplyRoute = (request, reply) => {
 
     //return request.body.input.num1 * request.body.input.num2
 
-    if (request.body.input.num1 === 5)    {
+    /*if (request.body.input.num1 === 5)    {
         reply.status(400).send({
             message: 'We  do not like n.5',            
         });
-    }
+    }*/
 
 
     let resultado = 0;
     if (request.body.input.operacion =="/") {
-        resultado = parseInt(request.body.input.num1) / parseInt(request.body.input.num2);
+        resultado = request.body.input.num1 / request.body.input.num2;
     } else if (request.body.input.operacion == "*") {
-        resultado = parseInt(request.body.input.num1) * parseInt(request.body.input.num2);
+        resultado = request.body.input.num1 * request.body.input.num2;
     } else if (request.body.input.operacion == "-") {
-        resultado = parseInt(request.body.input.num1) - parseInt(request.body.input.num2);
+        resultado = request.body.input.num1 - request.body.input.num2;
     } else {
-        resultado = parseInt(request.body.input.num1) + parseInt(request.body.input.num2);
+        resultado = request.body.input.num1 + request.body.input.num2;
     }        
 
     reply.send ({resultado});    
